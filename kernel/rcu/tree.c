97b15bfd6a36c217fe611d72d7653cc70248978e
Jules Irenge <jbi.octave@gmail.com>
2020-03-30 02:24:48 +0100

    rcu: Replace 1 by true
    Coccinelle reports a warning at use_softirq declaration
    
    WARNING: Assignment of 0/1 to bool variable
    
    The root cause is
    use_softirq a variable of bool type is initialised with the integer 1
    Replacing 1 with value true solve the issue.
    
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -133,6 +133,9 @@ static struct rcu_state rcu_state = {
 /* Dump rcu_node combining tree at boot to verify correct setup. */
 static bool dump_tree;
 module_param(dump_tree, bool, 0444);
+/* By default, use RCU_SOFTIRQ instead of rcuc kthreads. */
+static bool use_softirq = true;
+module_param(use_softirq, bool, 0444);
 /* Control rcu_node-tree auto-balancing at boot time. */
 static bool rcu_fanout_exact;
 module_param(rcu_fanout_exact, bool, 0444);
