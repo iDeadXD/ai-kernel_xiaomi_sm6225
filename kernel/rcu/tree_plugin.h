03423b4acfd579f4a347b455080f5fdc69ad55e3
Paul E. McKenney <paulmck@kernel.org>
2020-04-10 17:05:22 -0700

    rcu: Convert rcu_nohz_full_cpu() ULONG_CMP_LT() to time_before()
    This commit converts the ULONG_CMP_LT() in rcu_nohz_full_cpu() to
    time_before() to reflect the fact that it is comparing a timestamp to
    the jiffies counter.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -2839,8 +2839,8 @@ static bool rcu_nohz_full_cpu(struct rcu_state *rsp)
 {
 #ifdef CONFIG_NO_HZ_FULL
 	if (tick_nohz_full_cpu(smp_processor_id()) &&
-	    (!rcu_gp_in_progress(rsp) ||
-	     ULONG_CMP_LT(jiffies, READ_ONCE(rsp->gp_start) + HZ)))
+	    (!rcu_gp_in_progress() ||
+	     time_before(jiffies, READ_ONCE(rcu_state.gp_start) + HZ)))
 		return true;
 #endif /* #ifdef CONFIG_NO_HZ_FULL */
 	return false;
