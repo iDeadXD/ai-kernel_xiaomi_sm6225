a79353c9d653a0e8b9e63d303f3f75e9ef30b91b
Paul E. McKenney <paulmck@kernel.org>
2020-08-20 11:26:14 -0700

    rcu: Remove unused "cpu" parameter from rcu_report_qs_rdp()
    The "cpu" parameter to rcu_report_qs_rdp() is not used, with rdp->cpu
    being used instead.  Furtheremore, every call to rcu_report_qs_rdp()
    invokes it on rdp->cpu.  This commit therefore removes this unused "cpu"
    parameter and converts a check of rdp->cpu against smp_processor_id()
    to a WARN_ON_ONCE().
    
    Reported-by: Jann Horn <jannh@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2663,13 +2663,14 @@ rcu_report_unblock_qs_rnp(struct rcu_state *rsp,
  * structure.  This must be called from the specified CPU.
  */
 static void
-rcu_report_qs_rdp(int cpu, struct rcu_state *rsp, struct rcu_data *rdp)
+rcu_report_qs_rdp(struct rcu_data *rdp)
 {
 	unsigned long flags;
 	unsigned long mask;
 	bool needwake;
 	struct rcu_node *rnp;
 
+	WARN_ON_ONCE(rdp->cpu != smp_processor_id());
 	rnp = rdp->mynode;
 	raw_spin_lock_irqsave_rcu_node(rnp, flags);
 	if (rdp->cpu_no_qs.b.norm || rdp->gp_seq != rnp->gp_seq ||
@@ -2687,8 +2688,7 @@ rcu_report_qs_rdp(int cpu, struct rcu_state *rsp, struct rcu_data *rdp)
 		return;
 	}
 	mask = rdp->grpmask;
-	if (rdp->cpu == smp_processor_id())
-		rdp->core_needs_qs = false;
+	rdp->core_needs_qs = false;
 	if ((rnp->qsmask & mask) == 0) {
 		raw_spin_unlock_irqrestore_rcu_node(rnp, flags);
 	} else {
@@ -2738,7 +2738,7 @@ rcu_check_quiescent_state(struct rcu_state *rsp, struct rcu_data *rdp)
 	 * Tell RCU we are done (but rcu_report_qs_rdp() will be the
 	 * judge of that).
 	 */
-	rcu_report_qs_rdp(rdp->cpu, rsp, rdp);
+	rcu_report_qs_rdp(rdp);
 }
 
 /*
