e59a0e5217f722b94afbf40c0f8f94c6849b24c5
simonpunk <simonpunk2016@gmail.com>
2025-02-03 12:21:43 +0000

    fs: susfs: Update to 1.5.4-250203
    - kernel: SUS_MOUNT: fix normal mounts that are carelessly hidden in mountinfo
    - kernel: SUS_PATH: remove redundant inode_permission check; Add two more sus path hooks to compat_fillonedir() and compat_filldir() respectively
    
    * https://gitlab.com/simonpunk/susfs4ksu/-/commit/968706b8d0790dc8df174ec61d4edfa25600b527
    
    Signed-off-by: An. <noegrahachan@gmail.com>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -111,7 +111,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt.mnt_root->d_inode->i_state & INODE_STATE_SUS_MOUNT) && !susfs_is_current_ksu_domain()))
+	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
@@ -152,7 +152,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt.mnt_root->d_inode->i_state & INODE_STATE_SUS_MOUNT) && !susfs_is_current_ksu_domain()))
+	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
@@ -222,7 +222,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt.mnt_root->d_inode->i_state & INODE_STATE_SUS_MOUNT) && !susfs_is_current_ksu_domain()))
+	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
